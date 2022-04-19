9816ee6fd5c5bc7235272552f8adb2fd7ff95c64
xeinshamim <33855126+xeinshamim@users.noreply.github.com>
2022-04-20 04:55:43 +0600

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+import netlify from '../../content/thumbnails/netlify.png'
+import gatsby from '../assets/gatsby.png'
+import github from '../assets/nav-github.png'
+
+const links = [
+  { url: 'https://taniarascia.substack.com/subscribe', label: 'Newsletter' },
+  { url: 'https://ko-fi.com/taniarascia', label: 'Ko-Fi' },
+  { url: 'https://patreon.com/taniarascia', label: 'Patreon' },
+  { url: 'https://www.taniarascia.com/rss.xml', label: 'RSS' },
+]
+const madeWithLinks = [
+  { url: 'https://www.gatsbyjs.org/', label: 'Gatsby', icon: gatsby },
+  { url: 'https://github.com/taniarascia', label: 'GitHub', icon: github },
+  { url: 'https://www.netlify.com', label: 'Netlify', icon: netlify },
+]
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <section>
+        <nav>
+          <span className="desktop-only">Made by Tania Rascia</span>
+          {links.map((link) => (
+            <a
+              href={link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={link.url}
+            >
+              {link.label}
+            </a>
+          ))}
+        </nav>
+        <nav>
+          {madeWithLinks.map((link) => (
+            <a
+              href={link.url}
+              title={link.label}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={link.url}
+            >
+              <span>{link.label}</span>
+              <img src={link.icon} alt={link.label} />
+            </a>
+          ))}
+        </nav>
+      </section>
+    </footer>
+  )
+}
