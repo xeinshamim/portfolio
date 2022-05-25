d8cc15b76b94b7999fbad38acfbfbd9b3406a676
xeinshamim <33855126+xeinshamim@users.noreply.github.com>
2022-05-26 00:52:18 +0600

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-  siteTitle: 'Tania Rascia',
-  siteUrl: 'https://www.taniarascia.com',
+  siteTitle: 'Xein Shamim',
+  siteUrl: 'https://www.xein.netlify.com',
   siteLogo: '/logo.png',
   description:
     'Software engineer and open source creator. This is my digital garden.',
